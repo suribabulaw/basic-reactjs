973f4d3950028e5a45e94b2c4a70153f818bc5b6
Python & Django & React.JS <stackmern@gmail.com>
2018-11-29 14:39:36 +0530

    sample

---
+++ instance.py
@@ -0,0 +1,8 @@
+class Inst:
+    static_variable_name  = 'justAgile'
+    static_variable_contact =8008143721
+    @staticmethod
+    def static_method():
+        suri = print('suri')
+
+Inst.static_method()
